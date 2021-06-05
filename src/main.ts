83ba4e1a78e58f218954e36d5a8c7cf5d3272756
Nishant Padhi <nishantpadhi2015@gmail.com>
2021-06-05 09:17:13 +0530

    Files added

---
+++ src/main.ts
@@ -0,0 +1,30 @@
+import App from "./App.vue";
+import {BootstrapVue, IconsPlugin} from 'bootstrap-vue';
+import VueRouter from 'vue-router';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import moment from 'moment';
+import Vue from "vue";
+import Home from './components/Home.vue';
+import Registration from './components/Registration.vue';
+
+Vue.prototype.moment = moment;
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+Vue.use(VueRouter);
+
+Vue.config.productionTip = false;
+const router = new VueRouter({
+  routes: [
+    {path: '/', component: Home},
+    {path: '/Registration', component: Registration},
+  ]
+});
+
+new Vue({
+  router,
+  render: h => h(App),
+}).$mount('#app')
+
+
+
